f5c83e4fab59ffb332ad1431ded11f52e54e441f
TheUnquiet <zanerebouded@gmail.com>
2024-11-26 12:56:46 +0100

    Labo 1 - Member output dto properties added

--- Assembly.Rest/Dto/Output/MemberOutputDto.cs
+++ Assembly.Rest/Dto/Output/MemberOutputDto.cs
@@ -1,15 +1,21 @@
-﻿namespace Assembly.Rest.Dto.Output
+﻿using Assembly.Domain.Models;
+
+namespace Assembly.Rest.Dto.Output
 {
     public class MemberOutputDto
     {
-        public string FirstName { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        
+        public string LastName { get; set; } = string.Empty;
 
-        public string LastName { get; set; }
+        public string Email { get; set; } = string.Empty;
 
-        public string Email { get; set; }
-
-        public string Address { get; set; }
+        public string Address { get; set; } = string.Empty;
 
         public DateOnly Birthday { get; set; }
+
+        public string Intrest { get; set; } = string.Empty;
+
+        public MemberTypeDomain MemberType { get; set; }
     }
 }
